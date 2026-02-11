0576ed816059714abe45cce26ece12e2b068b416
jackan04 <jacob.asker@icloud.com>
2026-02-11 07:14:59 +0100

    - created ExperienceCards component - applied more global css styling - created home.jsx component

---
+++ src/components/experience-cards/ExperienceCard.jsx
@@ -0,0 +1,34 @@
+import styles from "./ExperienceCards.module.css";
+
+function Card({ label, url }) {
+  return (
+    <div className={styles.card}>
+      <a className="has-hover" href={url} target="_blank">
+        {label}
+      </a>
+    </div>
+  );
+}
+
+export default function ExperienceCards() {
+  return (
+    <div className={styles.cardsContainer}>
+      <Card
+        label="JavaScript"
+        url="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+      />
+      <Card
+        label="Swift & SwiftUI"
+        url="https://developer.apple.com/documentation/swiftui"
+      />
+      <Card label="NodeJS" url="https://nodejs.org/docs/latest/api/" />
+      <Card label="React" url="https://react.dev/" />
+      <Card label="React Native" url="https://reactnative.dev/" />
+      <Card label="C#" url="https://learn.microsoft.com/en-us/dotnet/csharp/" />
+      <Card
+        label="SQL"
+        url="https://developer.mozilla.org/en-US/docs/Glossary/SQL"
+      />
+    </div>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,42 @@
 import profileImage from "../assets/images/profile.jpeg";
+import { Link } from "react-router-dom";
+import ExperienceCards from "../components/experience-cards/ExperienceCard";
 
 export default function Home() {
   return (
-    <img
-      width={100}
-      height={100}
-      src={profileImage}
-      loading="lazy"
-      alt="Profile Image"
-    />
+    <div className="main-content">
+      <div className="text-section">
+        <img
+          width={128}
+          height={128}
+          src={profileImage}
+          loading="lazy"
+          alt="Profile Image"
+          className="profile-image"
+        />
+        <h2>
+          Informatics student with a passion for building and designing digital
+          things.
+        </h2>
+      </div>
+      <div className="text-section">
+        <h2>About Me</h2>
+        <p>
+          I’m currently finishing my bacherlors degree in informatics, where I
+          spend most of my time exploring the intersection of code and user
+          experience. When I’m not at the keyboard, you’ll usually find me
+          hiking with friends, lifting at the gym, or behind a camera. Take a
+          look at some of my{" "}
+          <Link className="inline-link" to={"/projects"}>
+            projects
+          </Link>
+          .
+        </p>
+      </div>
+      <div className="text-section">
+        <h2>Technical Experience</h2>
+        <ExperienceCards></ExperienceCards>
+      </div>
+    </div>
   );
 }
