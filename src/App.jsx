5c24e7ab0bc539604d664cbbdfcce8440105751c
jackan04 <jacob.asker@icloud.com>
2026-02-15 21:22:13 +0100

    fixed typo in import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/header/header.jsx";
+import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
 function App() {
