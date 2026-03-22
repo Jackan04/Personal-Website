4fc5faf49fba0445c8e645b8c1a84bfbb2c06a09
jackan04 <jacob.asker@icloud.com>
2026-03-22 09:32:33 +0100

    refactor: move loading styles from Gallery.module.css to index.css; replace custom loading element with Loading component

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,89 +1,81 @@
 import { useEffect, useState } from "react";
 import styles from "./Gallery.module.css";
 import UnsplashService from "../../services/unsplashService.js";
-import SpinnerIcon from "../../assets/icons/spinner.svg?react";
+import Loading from "../Loading.jsx";
 import Link from "../Link.jsx";
-import Accordion from "../Accordion.jsx";
 
 export default function Gallery() {
-    const [images, setImages] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [hasMore, setHasMore] = useState(true);
-    const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        const fetchImages = async () => {
-            setLoading(true);
-            try {
-                const data = await UnsplashService.getAllImages(1);
-                setImages(data);
-            } catch (err) {
-                setImages([]);
-                if (import.meta.env.DEV) {
-                    console.error("Error fetching images:", err);
-                }
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchImages();
-    }, []);
-
-    async function handleLoadMore() {
-        const nextPage = page + 1;
-        const newImages = await UnsplashService.getAllImages(nextPage);
-
-        if (newImages.length === 0 || newImages.length < 12) {
-            setHasMore(false);
+  useEffect(() => {
+    const fetchImages = async () => {
+      setLoading(true);
+      try {
+        const data = await UnsplashService.getAllImages(1);
+        setImages(data);
+      } catch (err) {
+        setImages([]);
+        if (import.meta.env.DEV) {
+          console.error("Error fetching images:", err);
         }
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        setImages((prev) => [...prev, ...newImages]);
-        setPage(nextPage);
+    fetchImages();
+  }, []);
+
+  async function handleLoadMore() {
+    const nextPage = page + 1;
+    const newImages = await UnsplashService.getAllImages(nextPage);
+
+    if (newImages.length === 0 || newImages.length < 12) {
+      setHasMore(false);
     }
 
-    if (loading) {
-        return (
-            <div className={styles.loadingContainer}>
-                <SpinnerIcon className={styles.spinnerIcon} />
-                <p>Loading gallery...</p>
-            </div>
-        );
-    }
+    setImages((prev) => [...prev, ...newImages]);
+    setPage(nextPage);
+  }
 
-    if (!loading && images.length < 1) {
-        return (
-            <div className={styles.loadingContainer}>
-                <p>Failed to load the images from Unsplash.</p>
-            </div>
-        );
-    }
+  if (loading) return <Loading message="Loading gallery..." />;
 
+  if (!loading && images.length < 1) {
     return (
-        <>
-            <div className={styles.gallery}>
-                {images.map((image) => (
-                    <div key={image.id} className={styles.imageCard}>
-                        <Link href={image.links.html}>
-                            <img
-                                src={image.urls.small}
-                                loading="lazy"
-                                alt={image.alt_description ?? "Unsplash photo"}
-                            />
-                        </Link>
-                    </div>
-                ))}
-            </div>
-
-            {hasMore && (
-                <button
-                    disabled={!hasMore}
-                    onClick={handleLoadMore}
-                    className={`hover-opacity ${styles.loadMoreButton}`}
-                >
-                    Load More
-                </button>
-            )}
-        </>
+      <div className="loading-container">
+        <p>Failed to load the images from Unsplash.</p>
+      </div>
     );
+  }
+
+  return (
+    <>
+      <div className={styles.gallery}>
+        {images.map((image) => (
+          <div key={image.id} className={styles.imageCard}>
+            <Link href={image.links.html}>
+              <img
+                src={image.urls.small}
+                loading="lazy"
+                alt={image.alt_description ?? "Unsplash photo"}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+
+      {hasMore && (
+        <button
+          disabled={!hasMore}
+          onClick={handleLoadMore}
+          className={`hover-opacity ${styles.loadMoreButton}`}
+        >
+          Load More
+        </button>
+      )}
+    </>
+  );
 }
