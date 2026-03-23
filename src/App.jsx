c7e93b35525d8605f2b7dc9bf8ee2e4761b3b484
jackan04 <jacob.asker@icloud.com>
2026-03-23 14:47:15 +0100

    replace contact button with envelope icon in footer; update import in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
-import Footer from "./components/footer/Footer.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
