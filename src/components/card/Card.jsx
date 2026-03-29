28ac0a6e5f5ca26b85e853019f3573c3b965fe57
jackan04 <jacob.asker@icloud.com>
2026-03-29 18:22:27 +0200

    replace module card hover style with hover-bg classname

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,7 +4,7 @@ import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 
 export default function Card({ title, desc, url }) {
   return (
-    <CustomLink href={url} className={styles.card}>
+    <CustomLink href={url} className={`hover-bg ${styles.card}`}>
       <div className="hstack justify-between">
         <h3>{title}</h3>
         <ChevronRightIcon className={`icon-small ${styles.chevron}`} />
